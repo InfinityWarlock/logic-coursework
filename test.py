821da7566e516edbbb777c8fb06e689605dd0788
InfinityWarlock <theo.hintonhallows123@gmail.com>
2021-04-28 20:12:47 +0100

    q5

---
+++ test.py
@@ -0,0 +1,3 @@
+l = [1, 2, 3, 2, 5]
+l = list(filter(lambda a: a != 2, l))
+print(l)
--- tvnj26.py
+++ tvnj26.py
@@ -1,3 +1,6 @@
+import copy
+clauses0=[[1, 2, 3], [4, 5, 6], [7, 8, 9], [-1, -2], [-1, -3], [-2, -3], [-4, -5], [-4, -6], [-5, -6], [-7, -8], [-7, -9], [-8, -9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [-1, -4], [-1, -7], [-4, -7], [-2, -5], [-2, -8], [-5, -8], [-3, -6], [-3, -9], [-6, -9], [1, 8], [-1, -8], [4, 2], [-2, -4]]
+
 def load_dimacs(filename):
     with open(filename, 'r') as f:
         l1 = f.readline()
@@ -19,8 +22,6 @@ def load_dimacs(filename):
                         clause.append(int(c))
                 clause_set.append(clause)
         return clause_set, N
-
-clauses0=[]
 
 def simple_sat_solve(clause_set):
     if clause_set == []:
@@ -51,11 +52,46 @@ def simple_sat_solve(clause_set):
             return truth_assignment
     return False
 
-
-
+def branching_sat_solve(clause_set, partial_assignment):
+    flat_list = []
+    for clause in clause_set:
+        flat_list.extend(clause)
+    variables = set()
+    for i in flat_list:
+        if i > 0:
+            variables.add(i)
+        else:
+            variables.add(0-i)
+    new_clause_set = copy.deepcopy(clause_set)
+    # print('1', new_clause_set, partial_assignment)
+    for i in partial_assignment:
+        if i in variables or 0-i in variables:
+            for clause in new_clause_set:
+                if i in clause:
+                    new_clause_set = list(filter(lambda a: a != clause, new_clause_set))
+                elif 0-i in clause:
+                    clause.remove(0-i) #do same filter thing above here and hope it works lol
+                    # new_clause_set[]
+            variables.discard(i)
+            variables.discard(0-i)
+    # print('2', new_clause_set, partial_assignment)
+    if variables == set():
+        if new_clause_set == []:
+            return partial_assignment
+        else:
+            return False
+    branch_var = list(variables)[0]
+    positive_partial_assignment = copy.copy(partial_assignment)
+    negative_partial_assignment = copy.copy(partial_assignment)
+    positive_partial_assignment.append(branch_var)
+    negative_partial_assignment.append(0-branch_var)
+    positive_branch_result = branching_sat_solve(new_clause_set, positive_partial_assignment)
+    negative_branch_result = branching_sat_solve(new_clause_set, negative_partial_assignment)
+    if positive_branch_result:
+        return positive_branch_result
+    if negative_branch_result:
+        return negative_branch_result
+    return False   
 
 print(simple_sat_solve(clauses0))
-if []:
-    print('hello')
-
-
+print(branching_sat_solve(clauses0, []))
